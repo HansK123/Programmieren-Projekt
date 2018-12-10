ae30a7cfc018f32d88c9fd83be75dd759fec8068
HansK123 <ansgarhandy123@gmail.com>
2018-12-10 18:55:12 +0100

    Trauriger Anfang

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import numpy as np
+import pypi as pp
 import sounddevice as sd
-
+import scipy
 if __name__ == "__main__":
     print("Hello World")
+
+scipy.io.wavfile.read(WavBeispiel.wav, mmap=False)
+
+
