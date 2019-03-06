f97fea087cf69842b0a60273d66eba88e271c8c5
HansK123 <45177010+HansK123@users.noreply.github.com>
2019-03-06 14:10:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,3 +30,23 @@ y_speed = librosa.effects.time_stretch(y, 0.9)
 y_pitched = librosa.effects.pitch_shift(y_speed, sr, n_steps=0)
 sd.play(y_pitched,sr,blocking=True)
 beat_frames lösen licht aus
+
+#LED RING
+	
+sudo apt-get update
+sudo apt-get install build-essential python-dev git scons swig  
+  
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+scons
+
+cd python
+sudo python setup.py install
+
+# LED strip configuration:
+LED_COUNT = 12 # Number of LED pixels.
+LED_PIN = 18 # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 5 # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 100 # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False # True to invert the signal (when using NPN transistor level shift)
