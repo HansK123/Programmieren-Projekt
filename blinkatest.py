6f9fd5630b4be1d3b3fb2c299e8c44aec5edabfc
HansK123 <ansgarhandy123@gmail.com>
2019-07-19 14:02:15 +0200

    hhhhh

--- blinkatest.py
+++ blinkatest.py
@@ -1,19 +1,5 @@
 import board
-import digitalio
-import busio
+import neopixel
+pixels = neopixel.NeoPixel(board.D18, 30)
 
-print("Hello blinka!")
-
-# Try to great a Digital input
-pin = digitalio.DigitalInOut(board.D4)
-print("Digital IO ok!")
-
-# Try to create an I2C device
-i2c = busio.I2C(board.SCL, board.SDA)
-print("I2C ok!")
-
-# Try to create an SPI device
-spi = busio.SPI(board.SCLK, board.MOSI, board.MISO)
-print("SPI ok!")
-
-print("done!")
+pixels[0] = (255, 0, 0)
