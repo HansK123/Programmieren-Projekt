b72b8e172689035f47fff6fa091c3449a538fe4e
HansK123 <ansgarhandy123@gmail.com>
2019-07-19 13:38:14 +0200

    hhhhh

--- app.py
+++ app.py
@@ -30,20 +30,9 @@ y_speed = librosa.effects.time_stretch(y, 0.9)
 y_pitched = librosa.effects.pitch_shift(y_speed, sr, n_steps=0)
 sd.play(y_pitched,sr,blocking=True)
 
-# LED strip configuration:
-LED_COUNT      = 12      # Number of LED pixels.
-LED_PIN        = 13      # GPIO pin connected to the pixels (must support PWM!).
-LED_CHANNEL    = 1       # PWM Channel must correspond to chosen LED_PIN PWM!
-LED_FREQ_HZ    = 800000  #  LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
-LED_BRIGHTNESS = 50     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+pixel_pin = board.NEOPIXEL
 
-...
+pixel_pin = board.D18
 
-# Main program logic follows:
-if __name__ == '__main__':
-        # Create NeoPixel object with appropriate configuration.
-        strip = Adafruit_NeoPixel(12, 18, 10, 0, 0, 10, 1)
-        # Intialize the library (must be called once before other functions).
-        strip.begin()
+num_pixels = 12
+pixels.fill((0, 255, 0))
