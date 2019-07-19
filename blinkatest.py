09e3ebd0fe9e7dba49de7048544a89cb25be77dd
HansK123 <ansgarhandy123@gmail.com>
2019-07-19 16:33:41 +0200

    hhhhh

--- blinkatest.py
+++ blinkatest.py
@@ -4,6 +4,9 @@ import time
 pixels = neopixel.NeoPixel(board.D18, 30)
 Ansgar=3
 pixels[Ansgar]=((255,255,0))
+time.sleep(5)
+pixels.fill((0,0,0))
+Ansgar=Ansgar+1
+time.sleep(5)
 
-time.sleep(10)
 pixels.fill((0,0,0))
