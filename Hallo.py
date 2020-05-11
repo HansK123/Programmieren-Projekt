7fd4f9dcf09d53e39d65b36207582a9d4e6c24cf
HansK123 <ansgarhandy123@gmail.com>
2020-05-11 17:54:36 +0200

    n

--- Hallo.py
+++ Hallo.py
@@ -22,8 +22,8 @@ print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
 # 4. Convert the frame indices of beat events into timestamps
 #beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-xyz = (60/12)
-xy = xyz/150
+#xyz = (60/12)
+#xy = xyz/150
 #print('Saving output to beat_times.csv')
 #librosa.output.times_csv('beat_times.csv', beat_times)
 sd.play(y,sr,blocking=True)
