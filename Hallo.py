418786ac4183a02e6d04b0250e335f51daeeb30c
HansK123 <ansgarhandy123@gmail.com>
2020-05-14 18:33:08 +0200

    n

--- Hallo.py
+++ Hallo.py
@@ -31,7 +31,7 @@ for i in range(12):
         pixels[i] = 1000
         time.sleep(xyz)     #Drehgeschwindigkeit
         pixels.show()
-
+        pixels.fill(0,0,0)
 
 sd.play(y,sr,blocking=True)
 
