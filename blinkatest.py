c4501e79cd6d21021accd654a5963a0c8a27f7fa
HansK123 <ansgarhandy123@gmail.com>
2020-05-14 18:23:34 +0200

    n

--- blinkatest.py
+++ blinkatest.py
@@ -1,13 +1,22 @@
 import board
 import neopixel
 import time
-pixels = neopixel.NeoPixel(board.D23, 30)
-Ansgar=3
-pixels[Ansgar]=((255,255,0))
-time.sleep(5)
-pixels.fill((0,0,0))
-Ansgar=Ansgar+1
-pixels[Ansgar]=((255,0,0))
-time.sleep(5)
 
-pixels.fill((0,0,0))
+pixels = neopixel.NeoPixel(board.D18, 12, brightness=0.1, auto_write=True)
+
+
+
+
+def theaterChaseRainbow(strip, wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+
+theaterChaseRainbow(strip, Color(127, 127, 127))
