a89cacea97aaa30c2629958f6aa928e1022f4c4a
HansK123 <ansgarhandy123@gmail.com>
2020-05-14 19:12:21 +0200

    n

--- Hallo.py
+++ Hallo.py
@@ -25,7 +25,7 @@ print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 #print('Saving output to beat_times.csv')
 #librosa.output.times_csv('beat_times.csv', beat_times)
 
-def
+
 for i in range(11):
         pixels[i] = (100,100,100)
         pixels.show()
