17a4292f18f19640c86107d77eed7fd66abad461
HansK123 <ansgarhandy123@gmail.com>
2020-05-14 18:27:38 +0200

    n

--- Hallo.py
+++ Hallo.py
@@ -26,13 +26,13 @@ xyz = (60/12/150)
 #print('Saving output to beat_times.csv')
 #librosa.output.times_csv('beat_times.csv', beat_times)
 
-sd.play(y,sr,blocking=True)
 
 for i in range(12):
         pixels[i] = 50  #Farbe
         time.sleep(xyz)     #Drehgeschwindigkeit
         pixels.show()
 
+sd.play(y,sr,blocking=True)
 
 
 
