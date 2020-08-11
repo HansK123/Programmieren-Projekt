99679577e6c323100c79694a79b911d5557f1e59
HansK123 <ansgarhandy123@gmail.com>
2020-08-11 15:12:45 +0200

    n

--- sound.py
+++ sound.py
@@ -15,5 +15,6 @@ tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
 print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
+
 # 4. Convert the frame indices of beat events into timestamps
 sd.play(y,sr,blocking=True)
