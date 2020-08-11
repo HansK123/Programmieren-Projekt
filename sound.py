625d71ce6ee9b6d854495fc589a4d37741822a4b
HansK123 <ansgarhandy123@gmail.com>
2020-08-11 14:50:11 +0200

    n

--- sound.py
+++ sound.py
@@ -8,7 +8,7 @@ filename = librosa.util.example_audio_file()
 
 # 2. Load the audio as a waveform `y`
 #    Store the sampling rate as `sr`
-y, sr = librosa.load(filename)
+y, sr = librosa.load(Beispiel2.wav)
 # 3. Run the default beat tracker
 tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
 
@@ -16,5 +16,4 @@ print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
 
 # 4. Convert the frame indices of beat events into timestamps
 beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-#playsound('Beispiel2')
 sd.play(y,sr,blocking=True)
