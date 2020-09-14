cb1d8255f2dff8df4c9e399a6034ada508099413
HansK123 <ansgarhandy123@gmail.com>
2020-09-14 12:40:50 +0200

    n

--- sound.py
+++ sound.py
@@ -1,20 +1,42 @@
-# Beat tracking example
-#import playsound
-import librosa
+import time
+import multiprocessing
+import board
+import neopixel
 import sounddevice as sd
+import librosa
+import librosa.display
+import numpy as np
 
-# 1. Get the file path to the included audio example
-filename = librosa.util.example_audio_file()
-
-# 2. Load the audio as a waveform `y`
-#    Store the sampling rate as `sr`
 y, sr = librosa.load("Demo.wav",sr=None)    # sr = none damit die sample rate des Songs übernommen wird
 
-# 3. Run the default beat tracker
-tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
+pixels = neopixel.NeoPixel(board.D18, 12, brightness=0.1, auto_write=True) # D18 ist der Pin des Raspberry, 12 die Anzahl der LED´s
 
-print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)        # Librosa funktion zum feststellen des Tempos
+
+#print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+
+def Ringlicht():                                                   #Funktion für das LED Ringlicht zur visualisierung der BPM
+        while True:                                             #While True damit das Ringlucht nicht nach einer Umdrehung stoppt. Noch keine                                                      #
+                for i in range(11):                             #bessere Lösung gefunden
+                        pixels[i] = (100, 100, 100)
+                        pixels.show()
+                        time.sleep(60 / 12 / 150)  # Drehgeschwindigkeit Proberechnung um die BPM (in diesem Fall 150) über das Ringlicht darzustellen
+                        pixels[i] = (0, 0, 0)
 
 
-# 4. Convert the frame indices of beat events into timestamps
-sd.play(y,sr,blocking=True)
+def musik_abspielen():                                    # Funktion zum Abspielen des Songs
+        sd.play(y, sr, blocking=True)
+
+
+
+p1 = multiprocessing.Process(target=musik_abspielen,args=[])
+p2 = multiprocessing.Process(target=ringlicht,args=[])
+
+if __name__ == '__main__':
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+finish 0 time.perf_counter()
+print("Fertig mit durchlauf nach")
